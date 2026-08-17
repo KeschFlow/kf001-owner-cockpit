15b0813e661faabea2e12e2babccee2080eb0776
Eddie’s World <keschflow@gmail.com>
2026-08-17 18:40:43 +0200

    feat: add public governance adapter configuration

---
+++ governance-config.js
@@ -0,0 +1,9 @@
+globalThis.KF001_CONFIG = Object.freeze({
+  apiBaseUrl: '',
+  ownerStatePath: '/v1/owner-state',
+  approvalIntentPath: '/v1/approval-intents',
+  pushSubscriptionPath: '/v1/push/subscriptions',
+  evidencePath: '/v1/evidence',
+  pushPublicKey: ''
+});
+
